f893e58bcf15cb22743890c5a9cb1c48e00afe5b
Imaad Uddin <imaaduddin27@gmail.com>
2021-11-01 00:16:00 -0500

    mergesort

---
+++ mergeSort.js
@@ -0,0 +1,11 @@
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    const middleIndex = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, middleIndex);
+    const rightArr = arr.slice(middleIndex, arr.length);
+
+    return merge(mergeSort(leftarr), mergeSort(rightArr));
+}
